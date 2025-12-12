497a7d66aff11cf9874a6ea3180a0190ba219e8f
NAVEENKUMAR SIVASANKARAN <107559422+NaveenJava98@users.noreply.github.com>
2025-12-12 15:46:50 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ class hello{
   public static void main(String[] args){
     System.out.println("Hello eclipse I am from github");
     System.out.println("I am from eclipse, nice to meet you github");
+    // recent changes made my Naveenkumar
     System.out.println("Thanks for the reply : from github");
   }
 }
