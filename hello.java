0932e03576e54d92586736a815444d62b1fb0378
NAVEENKUMAR SIVASANKARAN <107559422+NaveenJava98@users.noreply.github.com>
2025-12-12 16:23:40 +0530

    Java file from git

---
+++ hello.java
@@ -0,0 +1,8 @@
+class hello{
+	public static void main(String[] args){
+		System.out.println("Naveen here from Git");
+		System.out.println("Learned Git command for daily needs");
+		System.out.println("Still need to learn a lot");
+	}
+}
+
